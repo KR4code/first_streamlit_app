f6281fbbc7cf1e59088b769a4483ae3ffddf5a54
KR4code <104972228+KR4code@users.noreply.github.com>
2022-05-05 13:55:19 +0530

    Update streamlit_app.py
    Changed parents to mom in title 
    added header
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner')
+streamlit.title('My mom\'s new healthy diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('kale, Spinach & rocket smoothie')
+streamlit.text('Hard-boiled Free-range egg')
+                
