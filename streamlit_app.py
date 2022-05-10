a8df90ccf9cb01b177b3e92fe055c49f99e7fd68
KR4code <104972228+KR4code@users.noreply.github.com>
2022-05-11 00:59:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,9 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header('Fruityvice fruit Advice !')
-fruit_choice = streanlit.text_input('what fruit information would you like about?','Kiwi')
+fruit_choice = streanlit.text_input('what fruit  would you like information about?','Kiwi')
 streamlit.write('the user entered',fruit_choice)
 
 import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+"fruit_choice")
 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
